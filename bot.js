900f4c1fef786b639203007262cf1967c6ca71a3
mrmilomilo <49833750+mrmilomilo@users.noreply.github.com>
2020-05-05 22:04:31 +0200

    wip cmds

--- bot.js
+++ bot.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const Discord = require("discord.js")
 const client = new Discord.Client();
+const botname = 'ChooseForMe';
+
+var ItemsToChooseFrom = [];
 
 client.on("ready", () => {
 
@@ -11,11 +14,63 @@ client.on("ready", () => {
 
 
 client.on("message", msg => {
+  const mentionedUsers = msg.mentions.users;
+  
+  //@ChooseForMe cmd arg1  
+  if( !mentionedUsers.first() || !mentionedUsers.first().username  ) return;
 
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
+
+  const args = msg.content.split(/ +/);
+  const cmd = args[1];
+
+  console.log('cmd: ' + cmd);
+  console.log('args: ' + args);
+
+  switch(cmd) {
+    case "add":
+      
+      if(args[2]) {
+        const item = args[2];
+
+        ItemsToChooseFrom.push(item);
+        
+        console.log('Item added: ' + item);
+      }
+      else {
+        cmd = "syntaxerror";
+      }
+
+      break;
+    case "list":
+
+        console.log("Items to choose from: " + ItemsToChooseFrom);
+
+      break;
+    case "remove":
+
+        if(args[2]) {
+          const item = args[2];
+
+          ItemsToChooseFrom = ItemsToChooseFrom.filter( i => i!==item);
+
+          console.log("Removed " + item);
+        }
+      
+      break;
+    case "pick":
+
+      const picked = ItemsToChooseFrom[Math.floor(Math.random() * ItemsToChooseFrom.length)];
+      
+      console.log("Picked " + picked);
+
+      break;
+    default:
+      console.log('unrecognized cmd received: ' + cmd)
+      break;
   }
 
+  
 });
 
 client.login(process.env.BOT_TOKEN);
+
