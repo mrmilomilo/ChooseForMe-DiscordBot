45caa5822b9c0f2416a94355ccbf0eed06ce4221
mrmilomilo <49833750+mrmilomilo@users.noreply.github.com>
2020-05-06 18:11:57 +0200

    node persist integrated. list is saved on disk.

--- bot.js
+++ bot.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 
 const Discord = require("discord.js")
+const storage = require('node-persist');
+
 const client = new Discord.Client();
 const botname = 'ChooseForMe';
-
 var ItemsToChooseFrom = [];
+const StorageKey = 'itemsToChooseFrom';
+
 
 client.on("ready", () => {
 
@@ -22,7 +25,7 @@ client.on("message", msg => {
   if( !mentionedUsers.first() || !mentionedUsers.first().username  ) return;
 
   console.log('\n');
-  console.log("[" + new Date().toISOString() + "]" + msg.content);
+  console.log("[" + new Date().toISOString() + "]", msg.content);
 
   const args = msg.content.split(/ +/);
   const cmd = args[1] ? args[1].toLowerCase() : "choose";
@@ -40,6 +43,8 @@ client.on("message", msg => {
         ItemsToChooseFrom.push(item);
         
         console.log('Item added: ' + item);
+
+        SaveItemsToStorage(StorageKey, ItemsToChooseFrom);
       }
       else {
         cmd = "syntaxerror";
@@ -59,6 +64,8 @@ client.on("message", msg => {
           ItemsToChooseFrom = ItemsToChooseFrom.filter( i => i!==item);
 
           console.log("Removed " + item);
+
+          SaveItemsToStorage(StorageKey, ItemsToChooseFrom);
         }
       
       break;
@@ -71,6 +78,12 @@ client.on("message", msg => {
       console.log("Picked " + picked);
 
       break;
+    case "_wipe":
+        WipeStorage();  
+      break;
+    case "_printstorage":
+        PrintStorage();
+      break;
     default:
       console.log('unrecognized cmd received: ' + cmd)
       break;
@@ -79,5 +92,50 @@ client.on("message", msg => {
   
 });
 
+
+
+
 client.login(process.env.BOT_TOKEN);
 
+(async () => {
+  
+  await storage.init();
+
+  LoadItemsFromStorage(StorageKey);
+
+})();
+
+
+async function SaveItemsToStorage(key, value) {
+  let values = await storage.setItem(key, value);
+  
+  console.log('saving ', key, values);
+}
+
+
+async function LoadItemsFromStorage(key) {
+  let savedValues = await storage.getItem(key);
+ 
+  console.log('read from storage: ', savedValues);
+
+  if(savedValues) {
+    savedValues.forEach(e => ItemsToChooseFrom.push(e));
+  }
+  
+}
+
+//delete all storage content!
+async function WipeStorage() {
+  
+  ItemsToChooseFrom = [];
+
+  await storage.clear();
+
+  console.log('storage wiped.')
+}
+
+
+async function PrintStorage() {
+  let valuesInStorage = await storage.values();
+  console.log(valuesInStorage);
+}
